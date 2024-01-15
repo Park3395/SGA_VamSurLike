afd58af486c66b783fd6477ca9194a82ca4c37be
ssssangmin <ksm980609@naver.com>
2024-01-15 17:42:37 +0900

    Revert "1/15"
    This reverts commit 86bc436b1abdd289c977bf0f48d8bd7c88b08b8c.

--- Assets/Scripts/BeetleFSM.cs
+++ Assets/Scripts/BeetleFSM.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BeetleFSM : MonoBehaviour
 {
@@ -16,17 +15,8 @@ public class BeetleFSM : MonoBehaviour
 
     BeetleState e_State;
 
-    // Beetle 정보
-    public int hp = 80;
-    public int maxhp = 80;
-    public int attackPower = 12;
-    // HP 슬라이더
-    public Slider hpSlider;
-
-    // 플레이어 인식 범위
     public float findDistance = 10.0f;
 
-    // 플레이어 공격 범위
     public float attackDistance = 2.0f;
 
     Animator anim;
@@ -59,15 +49,12 @@ public class BeetleFSM : MonoBehaviour
                 Attack();
                 break;
             case BeetleState.Hurt:
-                // Hurt();
+                Hurt();
                 break;
             case BeetleState.Death:
-                // Death();
+                //Damaged();
                 break;
         }
-
-        // HP슬라이더에 현재 체력 반영
-        hpSlider.value = (float)hp / (float)maxhp;
     }
 
     void Spawn()
@@ -88,31 +75,27 @@ public class BeetleFSM : MonoBehaviour
 
     void Run()
     {
-        // 플레이어와의 거리가 공격 범위 밖이면 플레이어를 향해 이동
         if (Vector3.Distance(transform.position, player.position) > attackDistance)
         {
             Debug.Log("Run");
-
             //이동 방향
             Vector3 dir = (player.position - transform.position).normalized;
 
             //플레이어를 향해 방향 전환
             transform.forward = dir;
         }
-        // 플레이어와의 거리가 공격 범위 내라면 공격 상태로 전환
         else if (Vector3.Distance(transform.position, player.position) <= attackDistance)
         {
             Debug.Log("Attack");
             e_State = BeetleState.Attack;
         }
     }
-
-    void Attack()
+        void Attack()
     {
         // 플레이어가 공격 범위 내라면 공격을 시작한다
         if (Vector3.Distance(transform.position, player.position) <= attackDistance)
         {
-            anim.SetTrigger("RunToAttack");
+            anim.Play("Attack");
         }
         // 공격 범위를 벗어났다면 현재 상태를 Move로 전환한다 (재추격)
         else
@@ -120,12 +103,6 @@ public class BeetleFSM : MonoBehaviour
             e_State = BeetleState.Run;
             anim.SetTrigger("AttackToRun");
         }
-    }
-
-    public void AttackAction()
-    {
-        // 플레이어가 완성 된 후 수정사항. 데미지 처리 함수
-        // player.GetComponent<PlayerMove>().DamageAction(attackPower);
     }
 
     // 피격 상태
@@ -145,36 +122,7 @@ public class BeetleFSM : MonoBehaviour
         e_State = BeetleState.Run;
     }
 
-    public void HitEnemy(int hitPower)
-    {
-        if (e_State == BeetleState.Hurt || e_State == BeetleState.Death)
-        {
-            return;
-        }
-
-        // 플레이어의 공격력만큼 적 체력 감소
-        hp -= hitPower;
-        
-        if (hp > 0)
-        {
-            e_State = BeetleState.Hurt;
-
-            // 피격 애니메이션
-            anim.SetTrigger("Hurt");
-            Hurt();
-        }
-        else
-        {
-            e_State = BeetleState.Death;
-
-            // 사망 애니메이션
-            anim.SetTrigger("Death");
-            Death();
-        }
-
-    }
-
-    void Death()
+    void Die()
     {
         // 진행 중인 피격 코루틴 함수를 중지한다
         StopAllCoroutines();
