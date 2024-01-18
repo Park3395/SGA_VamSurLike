2f134117b23454682d43baf4e676d3aca4f4fb70
yeo <ygd08@naver.com>
2024-01-18 13:48:54 +0900

    lobby 0118-1
    dd

---
+++ Assets/LobbyScript/CursorManagement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//마우스 커서 제어 스크립트
+public class CursorManagement : MonoBehaviour
+{
+    [SerializeField] Texture2D OnCursor;
+    [SerializeField] Texture2D ExitCursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/LobbyScript/SelectCommando.cs
+++ Assets/LobbyScript/SelectCommando.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectCommando : MonoBehaviour
 {
+    //로비를 구성하는 오브젝트들 모음
     public GameObject lobby_commando;
     public GameObject commando_information;
     public GameObject readyicon;
@@ -11,8 +13,10 @@ public class SelectCommando : MonoBehaviour
     public GameObject huntress_information;
     public GameObject lobby_bandit;
     public GameObject bandit_information;
+    public GameObject lobby_gunner;
+    public GameObject gunner_information;
 
-
+    //클릭할 시 코만도의 정보와 모델을 제외한 다른 정보와 모델들을 없앤다
     public void OnClick()
     {
         commando_information.gameObject.SetActive(true);
@@ -22,6 +26,8 @@ public class SelectCommando : MonoBehaviour
         huntress_information.gameObject.SetActive(false);
         lobby_bandit.SetActive(false);
         bandit_information.SetActive(false);
+        lobby_gunner.SetActive(false);
+        gunner_information.SetActive(false);
 
     }
     
