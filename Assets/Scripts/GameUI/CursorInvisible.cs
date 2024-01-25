d7ed81ca2e618d6bbf08858bcf7bcd8876caa2ad
314 <lucky2489@naver.com>
2024-01-25 13:51:21 +0900

    아이템 ui 만드는중

---
+++ Assets/Scripts/GameUI/CursorInvisible.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorInvisible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ¸ÊÀÌ ÀÖ´Â ¾À¿¡¼­ Ä¿¼­¸¦ ¼û±è
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
