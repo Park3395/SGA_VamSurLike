a9719dd36669b56e52bbeb9bc217cb1d987b9702
JHP\lucky <lucky2489@naver.com>
2024-01-26 02:59:23 +0900

    0125집

--- Assets/Scripts/GameUI/ItemSelect.cs
+++ Assets/Scripts/GameUI/ItemSelect.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CursorInvisible : MonoBehaviour
+public class ItemSelect : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // ¸ÊÀÌ ÀÖ´Â ¾À¿¡¼­ Ä¿¼­¸¦ ¼û±è
-        Cursor.visible = false;
+        
     }
 
     // Update is called once per frame
