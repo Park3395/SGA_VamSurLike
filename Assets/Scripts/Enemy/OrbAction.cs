d400ad8274839104596da41f30f0991878ad9669
ssssangmin <ksm980609@naver.com>
2024-01-26 11:21:23 +0900

    orb action

--- Assets/Scripts/Enemy/OrbAction.cs
+++ Assets/Scripts/Enemy/OrbAction.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class OrbAction : MonoBehaviour
 {
-    VagrantFSM fsm;
-    PlayerStat p_Stat;
+    GameObject player;
+    public GameObject vagrant;
+    PlayerStat pStat;
+    VagrantFSM eFSM;
 
-    // 아무것도 충돌이 되지 않는 상태라 수정중 1/24
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        pStat = player.GetComponent<PlayerStat>();
+        eFSM = vagrant.GetComponent<VagrantFSM>();
+    }
+
+    
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Player")
         {
             Debug.Log("Collision with Player");
             // 닿았을 때 플레이어의 hp 감소
-            //p_Stat.NowHP -= fsm.attackPower;
+            pStat.NowHP -= eFSM.attackPower;
             Destroy(gameObject);
         }
 
