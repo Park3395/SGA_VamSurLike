e55a3f500498f7e3f04123bda36ebf97af25eca0
JHP\lucky <lucky2489@naver.com>
2024-01-29 07:12:30 +0900

    0128

--- Assets/Scripts/GameUI/ItemSelect.cs
+++ Assets/Scripts/GameUI/ItemSelect.cs
@@ -24,18 +24,33 @@ public class ItemSelect : MonoBehaviour
         buttonObjects = new Button[3];
 
         // 버튼 위치
-        Vector3 buttonPosition = allButtons[0].gameObject.transform.position;
+        Vector3 buttonPosition = new Vector3();
+        // 첫번째 버튼 위치
         buttonPosition.x = 480;
         buttonPosition.y = 540;
 
+        // 버튼 중복되지않도록 변수2개 생성해서 저장하고 같으면 다시 뽑기
+        int button1 =-1, button2=-1;
+
+        // 버튼 3개 생성
         for (int i = 0; i < 3; i++)
         {           
-            int buttonIndex = Random.Range(0, 5);    // 0이상 5미만의 랜덤값
+            // 11개 버튼중에서 랜덤으로
+            int buttonIndex = Random.Range(0, 11);    // 0이상 11미만의 랜덤값
+            if(i == 0)
+                button1 = buttonIndex;  //첫번째 버튼 index 저장
+            else if(i == 1)
+                button2 = buttonIndex;  //두번째 버튼 index 저장
+            while (buttonIndex == button1 || buttonIndex == button2)    // 같으면 다시 뽑기
+            { 
+                buttonIndex = Random.Range(0, 11);
+            }
+
             // 버튼 프리팹 Instantiate
             buttonObjects[i] = Instantiate(allButtons[buttonIndex], buttonPosition, Quaternion.identity);            
             buttonObjects[i].transform.SetParent(transform);    // 캔버스의 자식으로 설정해서 화면에 보이게
             buttonPosition.x += 480;                 // 다음 버튼 위치 조정
         }
     }
-
+    
 }
