ebe8f00afac1e79cb09c4626af34b9e3e96a7730
yeo <ygd08@naver.com>
2024-02-12 15:51:54 +0900

    일단 쉬프트 쿨타임 스크립트만 만들어 놓음
    ㅇㅇ

---
+++ Assets/Scripts/LobbyScript/ShiftCoolTime.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShiftCoolTime : MonoBehaviour
+{
+    public Text cooltime;
+    public Image image_fill;
+    public float time_cooltime = 3.0f;
+    public float time_current;
+    public float time_start;
+    private bool isEnded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
